258315858cd2b1727fb8ec53ec32fa3969dc743b
zhishopov <zhishopov@gmail.com>
2025-09-05 00:56:35 +0100

    Update index.ts to show the board

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import { State, Square } from './types';
+import { showBoard } from './board';
+
 function main(): void {
-    console.log("Welcome to Tic-Tac-Toe Game!");
+  console.log('Welcome to Tic-Tac-Toe Game!!!\n');
+
+  const emptyBoard: Square[] = Array<Square>(9).fill(null);
+
+  const initialState: State = {
+    board: emptyBoard,
+    currentPlayer: 'X',
+    movesCount: 0
+  };
+
+  const boardAsText: string = showBoard(initialState.board);
+  console.log(boardAsText);
+
+  console.log('\nCurrent player:', initialState.currentPlayer);
 }
 
 main();
