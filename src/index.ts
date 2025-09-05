bfc1440f04a4d4aec57bd20034b9f7c05694b927
Zhivko Shopov <zhishopov@gmail.com>
2025-09-05 10:34:44 +0100

    Refactor initial state

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,41 @@
-import { State, Square } from "./types";
+import { State, Square, Player } from "./types";
 import { showBoard } from "./board";
+import {
+  isValidMove,
+  performMove,
+  changePlayer,
+  checkWin,
+  isDraw,
+} from "./rules";
+import promptSync from "prompt-sync";
 
-function main(): void {
-  console.log("Welcome to Tic-Tac-Toe Game!!!\n");
+const prompt = promptSync();
 
-  // Each square is either X, O or null
+function createInitialState(): State {
   const emptyBoard: Square[] = Array<Square>(9).fill(null);
-
-  const initialState: State = {
+  return {
     board: emptyBoard,
     currentPlayer: "X",
-    movesCount: 0, // keep track of moves
+    movesCount: 0,
   };
-
-  const boardAsText: string = showBoard(initialState.board);
-  console.log(boardAsText);
-
-  console.log("\nCurrent player:", initialState.currentPlayer);
 }
 
-main();
+// function main(): void {
+//   console.log("Welcome to Tic-Tac-Toe Game!!!\n");
+
+//   // Each square is either X, O or null
+//   const emptyBoard: Square[] = Array<Square>(9).fill(null);
+
+//   const initialState: State = {
+//     board: emptyBoard,
+//     currentPlayer: "X",
+//     movesCount: 0, // keep track of moves
+//   };
+
+//   const boardAsText: string = showBoard(initialState.board);
+//   console.log(boardAsText);
+
+//   console.log("\nCurrent player:", initialState.currentPlayer);
+// }
+
+// main();
